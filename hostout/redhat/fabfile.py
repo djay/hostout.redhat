d2fd67baee7d70d4595f2824e8722885e9b5e196
Chris Shenton <chris@koansys.com>
2010-10-30 20:24:27 +0100

    fix yum pkg names for RedHat/Centos, add EPEL for python26

--- hostout/redhat/fabfile.py
+++ hostout/redhat/fabfile.py
@@ -1,6 +1,4 @@
-import os
-import os.path
-from fabric import api, contrib
+from fabric import api
 
 
 def bootstrap():
@@ -24,14 +22,18 @@ def bootstrap():
         api.sudo("echo 'Defaults:%%%(user)s !requiretty' >> /etc/sudoers"%locals(),pty=True)
 
 
+    # Redhat/centos don't have Python 2.6 or 2.7 in stock yum repos, use EPEL.
+    # Could also use RPMforge repo: http://dag.wieers.com/rpm/FAQ.php#B
+    api.sudo("rpm -Uvh --force http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-4.noarch.rpm")
 
 #    api.sudo('apt-get -y update')
 #    api.sudo('apt-get -y upgrade ')
     
     
     version = api.env['python-version']
-    major = '.'.join(version.split('.')[:2])
-
+    #major = '.'.join(version.split('.')[:2])
+    major = ''.join(version.split('.')[:2])
+    
     #api.sudo('yum --help')
     api.sudo('yum -y install gcc gcc-c++ '
             'openssl openssl-devel '
@@ -42,75 +44,52 @@ def bootstrap():
             'libxslt libxslt-devel ')
 
     api.sudo('yum -y install '
-             'build-essential '
-             'python%(major)s python%(major)s-dev '
+             'python%(major)s python%(major)s-devel '
              'python-setuptools '
-             'python-libxml2 '
+             'libxml2-python '
              'python-elementtree '
-#             'python-celementtree '
-             'ncurses-dev '
+             'ncurses-devel '
              'lynx '
              'python-imaging '
-             'libjpeg-dev '
-             'libfreetype6-dev '
-             'zlib1g-dev '
-             'libreadline5-dev '
-             'zlib1g-dev '
-             'libbz2-dev '
-             'libssl-dev '
-             'libjpeg62-dev '
+             'libjpeg-devel '
+             'freetype-devel '
+             'zlib-devel '
+             'readline-devel '
+             'bzip2-devel '
+             'openssl-devel '
+             'libjpeg-devel '
              % locals())
-    # python-profiler?
 
     #to install Python tools 2.4
     api.sudo('wget http://peak.telecommunity.com/dist/ez_setup.py')
     api.sudo('python%(major)s ez_setup.py' % locals())
 
     #to install PIL
-    api.sudo('easy_install-%(major)s --find-links http://download.zope.org/distribution PILwoTK' % locals())
-    api.sudo('easy_install-%(major)s --find-links http://dist.repoze.org/PIL-1.1.6.tar.gz PIL' % locals())
+    easy_install = "easy_install-" + '.'.join(version.split('.')[:2]) #easy_install-2.6
+    api.sudo(easy_install + ' --find-links http://download.zope.org/distribution PILwoTK' % locals())
+    api.sudo(easy_install + ' --find-links http://dist.repoze.org/PIL-1.1.6.tar.gz PIL' % locals())
 
-    #if its ok you will see something like this:
-    #--------------------------------------------------------------------
-
+    #if its ok you will see something like this but you need JPEG, PNG:
     #*** TKINTER support not available
 
-    #--- JPEG support ok
-
-    #--- ZLIB (PNG/ZIP) support ok
-
-    #--- FREETYPE2 support ok
-
-    #--------------------------------------------------------------------
-
     # Add the plone user:
-
     owner = api.env['buildout-user']
     effective = api.env['effective-user']
     buildoutgroup = api.env['buildout-group']
-    
+
     api.sudo('egrep %(owner)s /etc/passwd || adduser %(owner)s ' % locals())
     api.sudo('egrep %(effective)s /etc/passwd || adduser %(effective)s' % locals())
+    api.sudo('groupadd -f ' + buildoutgroup)
     api.sudo('gpasswd -a %(owner)s %(buildoutgroup)s' % locals())
     api.sudo('gpasswd -a %(effective)s %(buildoutgroup)s' % locals())
 
 
-    hostout.setaccess()
-
     path = api.env.path
     api.sudo('mkdir -p %(path)s' % locals())
     hostout.setowners()
-    
+
     #install buildout
-#    api.sudo('easy_install-%(major)s zc.buildout' % locals())
     api.env.cwd = api.env.path
     api.sudo('wget http://svn.zope.org/*checkout*/zc.buildout/trunk/bootstrap/bootstrap.py')
     api.sudo('echo "[buildout]" > buildout.cfg')
     api.sudo('python%(major)s bootstrap.py' % locals())
-
-#    api.sudo('buildout init')
-#    api.run('cd /%(path)s && bin/buildout install lxml' % locals())
-#    api.run('cd /%(path)s && bin/buildout' % locals())
-
-
-
